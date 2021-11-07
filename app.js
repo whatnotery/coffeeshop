7f244a4e91ebf825a6217e72a6dbe384aeb560ea
Jos O'shea <jos.oshea.ky@gmail.com>
2021-11-07 17:51:34 -0500

    turned into single page app

---
+++ app.js
@@ -0,0 +1,21 @@
+const toggle = document.getElementById('toggle')
+const home = document.getElementById('home-wrap')
+const menu = document.getElementById('menu-wrap')
+
+
+function switchContent() {
+    if (menu.classList.contains('hide')) {
+        home.classList.add('hide')
+        menu.classList.remove('hide')
+        toggle.innerText = "home"
+    } else if (home.classList.contains('hide')) {
+        menu.classList.add('hide')
+        home.classList.remove('hide')
+        toggle.innerText = "menu"
+    }
+
+}
+
+
+toggle.addEventListener('click', switchContent)
+
